1020eaaedf061080d413962a069645f3b2ee928b
saranyashanmugam04 <37809422+saranyashanmugam04@users.noreply.github.com>
2019-06-07 13:19:10 +0530

    leanft projects

--- src/main/java/unittesting/UnitTestClassBase.java
+++ src/main/java/unittesting/UnitTestClassBase.java
@@ -1,75 +1,76 @@
 package unittesting;
 
-import java.lang.reflect.Method;
-import java.util.Iterator;
-
-import org.testng.ITestContext;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.DataProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
 
 import com.hp.lft.report.Status;
-import com.hp.lft.unittesting.TestNgUnitTestBase;
+import com.hp.lft.unittesting.UnitTestBase;
 
-public class UnitTestClassBase extends TestNgUnitTestBase {
-    
-    @BeforeSuite(alwaysRun=true)
-    public void baseBeforeSuite() throws Exception{
-        suiteSetup();
+public class UnitTestClassBase extends UnitTestBase {
+
+    protected static UnitTestClassBase instance;
+
+    public static void globalSetup(Class<? extends UnitTestClassBase> testClass) throws Exception {
+        if (instance == null)
+            instance = testClass.newInstance();
+        instance.classSetup();
     }
 
-    @BeforeClass(alwaysRun=true)
-    public void baseBeforeClass(ITestContext ctx) throws Exception {
-        String[] context = { ctx.getSuite().getName(), ctx.getName(), this.getClass().getName() };
-        className.set(this.getClass().getName());
-        classSetup(context);
+    @Before
+    public void beforeTest() throws Exception {
+        testSetup();
     }
 
-    @BeforeMethod(alwaysRun=true)
-    public void baseBeforeMethod(Object[] arguments, ITestContext ctx, Method method) throws Exception {
-        String methodName = super.getMethodName(method, arguments);
-        String[] context = { ctx.getSuite().getName(), ctx.getName(), this.getClass().getName(), methodName };
-        testName.set(methodName);
-        testSetup(context);
+    @After
+    public void afterTest() throws Exception {
     }
 
-    @AfterMethod(alwaysRun=true)
-    public void baseAfterMethod(ITestResult result) throws Exception {
-        setStatus(result.getStatus() == ITestResult.FAILURE ? Status.Failed : Status.Passed);
-        testTearDown();
-    }
-
-    @AfterClass(alwaysRun=true)
-    public void baseAfterClass(ITestContext ctx) throws Exception {
-        classTearDown();
-    }
-
-    @AfterSuite(alwaysRun=true)
-    public void baseAfterSuite() throws Exception {
-        suiteTearDown();
+    public static void globalTearDown() throws Exception {
+        instance.classTearDown();
         getReporter().generateReport();
     }
 
+    @ClassRule
+    public static TestWatcher classWatcher = new TestWatcher() {
+        @Override
+        protected void starting(org.junit.runner.Description description) {
+            className = description.getClassName();
+        }
+    };
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void starting(org.junit.runner.Description description) {
+            testName = description.getMethodName();
+        }
+        @Override
+        protected void failed(Throwable e, org.junit.runner.Description description) {
+            setStatus(Status.Failed);
+            testTearDownNoThrow();
+        }
+        
+        @Override
+        protected void succeeded(org.junit.runner.Description description) {
+            setStatus(Status.Passed);
+            testTearDownNoThrow();
+        }
+    };
+
     @Override
     protected String getTestName() {
-        return testName.get();
+        return testName;
     }
-
+    
     @Override
     protected String getClassName() {
-        return className.get();
+        return className;
     }
 
-    @DataProvider(name="ALM")
-    public Iterator<String[]> data() throws Exception {
-        return getAlmData().iterator(); 
-    }
-
-    protected ThreadLocal<String> className = new ThreadLocal<String>();
-    protected ThreadLocal<String> testName = new ThreadLocal<String>();
+    protected static String className;
+    protected String testName;
+    
 }
